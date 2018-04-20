897bc539477362eb437c21399cedab02e5861848
mikhail.shchegolev <mikhail.shchegolev@MacBook-Pro-mikhailshchegolev.local>
2018-04-20 22:27:10 +0300

    init project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='peny',
+      version='0.1',
+      description='Project enterprise numeric yaffil',
+      url='http://github.com/mshegolev/peny.git',
+      author='Mikhail Shchegolev',
+      author_email='mshegolev@gmail.com',
+      license='MIT',
+      packages=['peny'],
+      zip_safe=False)
